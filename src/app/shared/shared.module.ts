144cb4aab8f7648f3fef2ad7956056bc7d7494fe
Diego <diegopaginidev@gmail.com>
2021-06-14 12:15:37 -0300

    spinner

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { HeaderModule } from './header/header.module';
 import { FooterModule } from './footer/footer.module';
 import { ProductCardModule } from './product-card/product-card.module';
 import { CarouselModule } from './carousel/carousel.module';
+import { SpinnerModule } from './spinner/spinner.module';
 
 @NgModule({
   imports: [
@@ -12,7 +13,14 @@ import { CarouselModule } from './carousel/carousel.module';
     FooterModule,
     ProductCardModule,
     CarouselModule,
+    SpinnerModule,
   ],
-  exports: [HeaderModule, FooterModule, ProductCardModule, CarouselModule],
+  exports: [
+    HeaderModule,
+    FooterModule,
+    ProductCardModule,
+    CarouselModule,
+    SpinnerModule,
+  ],
 })
 export class SharedModule {}
