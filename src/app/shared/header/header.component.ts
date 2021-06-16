9ec912ada1f97ac3d9f472e0f4351c75c224b6e5
Diego <diegopaginidev@gmail.com>
2021-06-16 09:09:14 -0300

    products length in header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  public cartProductsLength$: Observable<any>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private store: Store<{ cart: any }>) {
+    this.cartProductsLength$ = this.store.select('cart').pipe(
+      map((cart) => cart.cart),
+      map((cartProducts) => cartProducts.length),
+      tap(console.log)
+    );
   }
-
 }
