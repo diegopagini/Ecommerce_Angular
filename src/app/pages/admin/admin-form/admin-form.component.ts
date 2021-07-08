3a7b770f407cbd0f325e46386b7168b1dfc9fadf
Diego <diegopaginidev@gmail.com>
2021-07-08 09:02:18 -0300

    add product service working

--- src/app/pages/admin/admin-form/admin-form.component.ts
+++ src/app/pages/admin/admin-form/admin-form.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { finalize } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-admin-form',
   templateUrl: './admin-form.component.html',
   styleUrls: ['./admin-form.component.scss'],
 })
-export class AdminFormComponent implements OnInit {
+export class AdminFormComponent {
   public adminForm: FormGroup;
   public categories: string[] = [
     'electronics',
@@ -14,8 +19,14 @@ export class AdminFormComponent implements OnInit {
     "men's clothing",
     "women's clothing",
   ];
+  private downloadUrl: Observable<string>;
+  private fbUrl: string;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private firebaseService: FirebaseService,
+    private storage: AngularFireStorage
+  ) {
     this.adminForm = this.fb.group({
       id: [this.randomizeId()],
       title: ['', [Validators.required, Validators.minLength(5)]],
@@ -25,8 +36,6 @@ export class AdminFormComponent implements OnInit {
       image: ['', [Validators.required]],
     });
   }
-
-  ngOnInit(): void {}
 
   get titleNoValid() {
     return (
@@ -60,13 +69,42 @@ export class AdminFormComponent implements OnInit {
     );
   }
 
+  onFileSelected(event) {
+    let n = Date.now();
+    const file = event.target.files[0];
+    const filePath = `img/${n}`;
+    const fileRef = this.storage.ref(filePath);
+    const task = this.storage.upload(`img/${n}`, file);
+    task
+      .snapshotChanges()
+      .pipe(
+        finalize(() => {
+          this.downloadUrl = fileRef.getDownloadURL();
+          this.downloadUrl.subscribe((url) => {
+            if (url) {
+              this.fbUrl = url;
+            }
+          });
+        })
+      )
+      .subscribe();
+  }
+
   randomizeId() {
     return Math.random().toString(36).substring(2);
   }
 
   submit() {
     if (this.adminForm.valid) {
-      console.log(this.adminForm.value);
+      this.adminForm.value.image = this.fbUrl;
+      this.firebaseService.addNewProduct(this.adminForm.value);
+      Swal.fire({
+        icon: 'success',
+        title: 'Your product was added successfully',
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      this.adminForm.reset();
     } else {
       this.adminForm.markAllAsTouched();
     }
